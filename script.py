87f674d17bc019e478585cc775e4f10422be673e
Bob Belderbos <bobbelderbos@gmail.com>
2023-05-23 11:41:25 +0200

    update tests + template code

--- script.py
+++ script.py
@@ -1,61 +1,92 @@
 import re
 
 
-def email_regex(string):
-    pass
-
-
-def url_regex(string):
-    pass
-
-
-def phone_number_regex(string):
-    pass
-
-
-def hello_world(string):
-    pass
-
-
 def lowercase_letters(string):
-    pass
+    """
+    Write a regular expression to match a string that contains only lowercase letters.
+    """
 
 
 def contains_digit(string):
-    pass
-
-
-def consecutive_vowels(string):
-    pass
+    """
+    Write a regular expression to match a string that contains at least one digit.
+    """
 
 
 def word_length(string):
-    pass
+    """
+    Write a regular expression to match a string that contains a word with exactly five letters.
+    """
 
 
-def consecutive_consonants(string):
-    pass
-
-
-def consecutive_repeating_characters(string):
-    pass
+def url_regex(string):
+    """
+    Write a regular expression to match a valid URL.
+    """
 
 
 def mixed_case_letters(string):
-    pass
+    """
+    Write a regular expression to match a string that contains a word with both uppercase and lowercase letters.
+    """
 
 
-def vowel_start_length(string):
-    pass
+def phone_number_regex(string):
+    """
+    Write a regular expression to match a phone number in a specific format, such as (123)456-7890.
+    """
 
 
-def repeated_word(string):
-    pass
+def consecutive_consonants(string):
+    """
+    Write a regular expression to match a string that contains a word with three or more consecutive consonants.
+    """
 
 
-def word_start_end_az(string):
-    pass
+def hello_world(string):
+    """
+    Write a regular expression to match a string that starts with "hello" and ends with "world".
+    """
+
+
+def consecutive_vowels(string):
+    """
+    Write a regular expression to match a string that contains three consecutive vowels.
+    """
 
 
 def word_start_end_ae_noz(string):
-    pass
+    """
+    Write a regular expression to match a string that contains a word that starts with "a" and ends with "e" and may have any number of characters in between, excluding "z".
+    """
+
+
+def email_regex(string):
+    """
+    Write a regular expression to match an email address.
+    """
+
+
+def vowel_start_length(string):
+    """
+    Write a regular expression to match a string that contains a word with a length of 7 or more characters and starts with a vowel.
+    """
+
+
+def consecutive_repeating_characters(string):
+    """
+    Write a regular expression to match a string that contains a word with two or more consecutive repeating characters.
+    """
+
+
+def repeated_word(string):
+    """
+    Write a regular expression to match a string that contains a word that is repeated consecutively at least twice.
+    """
+
+
+def word_start_end_az_and_one_digit(string):
+    """
+    Write a regular expression to match a string that contains a word starting
+    with "a" and ending with "z", with at least one digit in between.
+    """
--- test_script.py
+++ test_script.py
@@ -1,24 +1,93 @@
 import pytest
-
 from script import (
-    email_regex,
-    url_regex,
-    phone_number_regex,
-    hello_world,
     lowercase_letters,
     contains_digit,
-    consecutive_vowels,
     word_length,
-    consecutive_consonants,
-    consecutive_repeating_characters,
+    url_regex,
     mixed_case_letters,
-    vowel_start_length,
-    repeated_word,
-    word_start_end_az,
+    phone_number_regex,
+    consecutive_consonants,
+    hello_world,
+    consecutive_vowels,
     word_start_end_ae_noz,
+    email_regex,
+    vowel_start_length,
+    consecutive_repeating_characters,
+    repeated_word,
+    word_start_end_az_and_one_digit,
 )
 
-# Exercise 1
+
+def test_lowercase_letters():
+    assert lowercase_letters("abcd")
+    assert lowercase_letters("efgh")
+    assert not lowercase_letters("abcD")
+    assert not lowercase_letters("1234")
+
+
+def test_contains_digit():
+    assert contains_digit("abc123")
+    assert not contains_digit("abcd")
+    assert not contains_digit("efgh")
+
+
+def test_word_length():
+    assert word_length("apple")
+    assert not word_length("pear")
+    assert not word_length("grapefruit")
+
+
+def test_url_regex():
+    assert url_regex("https://www.example.com")
+    assert url_regex("http://example.org")
+    assert not url_regex("www.example.com")
+    assert not url_regex("http:/example.org")
+    assert not url_regex("example.org")
+
+
+def test_mixed_case_letters():
+    assert mixed_case_letters("HelloWorld")
+    assert mixed_case_letters("hellOworLd")
+    assert not mixed_case_letters("helloworld")
+    assert not mixed_case_letters("HELLO")
+    assert not mixed_case_letters("HEL3lo")
+
+
+def test_phone_number_regex():
+    assert phone_number_regex("(123)456-7890")
+    assert not phone_number_regex("(123)4567890")
+    assert not phone_number_regex("123456-7890")
+    assert not phone_number_regex("(123)456-7890abc")
+
+
+def test_consecutive_consonants():
+    assert consecutive_consonants("xyzbcdfg")
+    assert consecutive_consonants("bcd")
+    assert not consecutive_consonants("aeiou")
+
+
+def test_hello_world():
+    assert hello_world("hello world")
+    assert hello_world("hello123 world")
+    assert hello_world("hello123world")
+    assert hello_world("helloworld")
+    assert not hello_world("hi world")
+    assert not hello_world("hello universe")
+
+
+def test_consecutive_vowels():
+    assert consecutive_vowels("aiou")
+    assert consecutive_vowels("aeiou")
+    assert not consecutive_vowels("aeKio")
+    assert not consecutive_vowels("aeKiosae")
+
+
+def test_word_start_end_ae_noz():
+    assert word_start_end_ae_noz("apple")
+    assert word_start_end_ae_noz("ape")
+    assert not word_start_end_ae_noz("aze")
+
+
 def test_email_regex():
     assert email_regex("test@example.com")
     assert email_regex("user.name@example.co.uk")
@@ -26,96 +95,31 @@ def test_email_regex():
     assert not email_regex("test@example")
     assert not email_regex("test@example..com")
 
-# Exercise 2
-def test_url_regex():
-    assert url_regex("https://www.example.com")
-    assert url_regex("http://example.org")
-    assert url_regex("ftp://ftp.example.com")
-    assert not url_regex("www.example.com")
-    assert not url_regex("http:/example.org")
-    assert not url_regex("example.org")
 
-# Exercise 3
-def test_phone_number_regex():
-    assert phone_number_regex("(123)456-7890")
-    assert not phone_number_regex("(123)4567890")
-    assert not phone_number_regex("123456-7890")
-    assert not phone_number_regex("(123)456-7890abc")
+def test_vowel_start_length():
+    assert vowel_start_length("elephant")
+    assert vowel_start_length("OpenAI_is_an_incredible_platform_for_AI_development")
+    assert not vowel_start_length("apple")
+    assert not vowel_start_length("orange")
 
-# Exercise 4
-def test_hello_world():
-    assert hello_world("hello world")
-    assert hello_world("hello123 world")
-    assert not hello_world("hello123world")
-    assert not hello_world("helloworld")
 
-# Exercise 5
-def test_lowercase_letters():
-    assert lowercase_letters("abcd")
-    assert not lowercase_letters("abcD")
-    assert not lowercase_letters("1234")
-
-# Exercise 6
-def test_contains_digit():
-    assert contains_digit("abc123")
-    assert contains_digit("abcd")
-    assert not contains_digit("efgh")
-
-# Exercise 7
-def test_consecutive_vowels():
-    assert consecutive_vowels("aiou")
-    assert consecutive_vowels("aeiou")
-    assert not consecutive_vowels("aeio")
-
-# Exercise 8
-def test_word_length():
-    assert word_length("apple")
-    assert not word_length("pear")
-    assert not word_length("grapefruit")
-
-# Exercise 9
-def test_consecutive_consonants():
-    assert consecutive_consonants("xyzbcdfg")
-    assert consecutive_consonants("bcd")
-    assert not consecutive_consonants("aeiou")
-
-# Exercise 10
 def test_consecutive_repeating_characters():
     assert consecutive_repeating_characters("hello")
     assert consecutive_repeating_characters("goooood")
     assert not consecutive_repeating_characters("world")
+    assert not consecutive_repeating_characters("the station")
 
-# Exercise 11
-def test_mixed_case_letters():
-    assert mixed_case_letters("HelloWorld")
-    assert mixed_case_letters("helloworld")
-    assert not mixed_case_letters("HELLO")
 
-# Exercise 12
-def test_vowel_start_length():
-    assert vowel_start_length("apple")
-    assert vowel_start_length("elephant")
-    assert not vowel_start_length("orange")
-
-# Exercise 13
 def test_repeated_word():
     assert repeated_word("Hello Hello")
     assert repeated_word("How are you? How are you?")
     assert not repeated_word("No repetition here")
 
-# Exercise 14
-def test_word_start_end_az():
-    assert word_start_end_az("apple")
-    assert word_start_end_az("aardvark")
-    assert not word_start_end_az("banana")
 
-# Exercise 15
-def test_word_start_end_ae_noz():
-    assert word_start_end_ae_noz("apple")
-    assert word_start_end_ae_noz("ape")
-    assert not word_start_end_ae_noz("aze")
-
-# Run the tests
-if __name__ == '__main__':
-    pytest.main()
-
+def test_word_start_end_az_and_one_digit():
+    assert word_start_end_az_and_one_digit("a1z")
+    assert word_start_end_az_and_one_digit("a.2.z")
+    assert word_start_end_az_and_one_digit("a2anotherstring3z")
+    assert not word_start_end_az_and_one_digit("abz")
+    assert not word_start_end_az_and_one_digit("aanotherstringz")
+    assert not word_start_end_az_and_one_digit("a1k")
